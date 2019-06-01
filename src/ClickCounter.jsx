9246c378e4b80f36f2d8206b50504c7b09ae6dc2
Brian Zhang <brianbzhang@gmail.com>
2019-05-31 23:32:46 -0400

    click counter

---
+++ src/ClickCounter.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
+const ClickCounter = (props) => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  }, [count]);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button data-testid="click-counter-button" onClick={() => setCount(count => count + 1)}>
+        Click Me
+      </button>
+    </div>
+  )
+}
+
+export default ClickCounter;
---
+++ src/ClickCounter.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render, fireEvent, cleanup, waitForElement, } from '@testing-library/react';
+import ClickCounter from './ClickCounter';
+
+afterEach(cleanup);
+
+test('component updates count when clicked', () => {
+  const { getByText, getByTestId, asFragment } = render(
+    <ClickCounter />
+  );
+  fireEvent.click(getByText('Click Me'));
+  // const countTextNode = getByText(/You click.*\d.*times/i);
+  const countTextNode = getByTestId('click-counter-button');
+  expect(countTextNode).toHaveTextContent('You clicked 1 times');
+});
